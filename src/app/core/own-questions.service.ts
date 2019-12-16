da2c1295afb72636ff7f5dfed77de7d876b8d6a3
IlyaYudin <ilya.yudin@ledgerleopard.com>
2019-12-16 08:47:39 +0300

    Updating to Angular 8

--- src/app/core/own-questions.service.ts
+++ src/app/core/own-questions.service.ts
@@ -1,9 +1,12 @@
+
+import {of as observableOf,  Observable } from 'rxjs';
+
+import {tap} from 'rxjs/operators';
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Category} from '../models/questions.model';
-import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/observable/of';
-import 'rxjs/add/operator/do';
+
+
 
 
 @Injectable()
@@ -13,9 +16,9 @@ export class OwnQuestionsService {
     constructor(private http: HttpClient) { }
 
     loadCategories(): Observable<Category[]> {
-        if (this.categories) return Observable.of(this.categories);
-        return this.http.get<Category[]>('assets/questions.json')
-            .do((categories: Category[]) => this.categories = categories.map(data => Object.assign(new Category(), data)));
+        if (this.categories) return observableOf(this.categories);
+        return this.http.get<Category[]>('assets/questions.json').pipe(
+            tap((categories: Category[]) => this.categories = categories.map(data => Object.assign(new Category(), data))));
     }
 
     get hasQuestions(): boolean {
